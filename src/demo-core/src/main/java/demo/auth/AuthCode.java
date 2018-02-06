904350870615357cf00ec4f261d6e374b91f88e1
gouzhiguo <gouzhiguo@126.com>
2018-02-06 09:54:54 +0800

    添加功能权限

--- src/demo-core/src/main/java/demo/auth/AuthCode.java
+++ src/demo-core/src/main/java/demo/auth/AuthCode.java
@@ -9,11 +9,15 @@ public enum AuthCode {
     NO("无权限","No"),
     ALL("所有权限","ALL"),
 
-    //用户权限
+    //用户
     User0001("用户查看","User0001"),
     User0002("用户修改","User0002"),
-    User0003("用户修改状态","User0003");
-
+    User0003("用户修改状态","User0003"),
+    //功能权限
+    Permission0001("功能权限查看","Permission0001"),
+    Permission0002("功能权限修改","Permission0002"),
+    Permission0003("功能权限状态","Permission0003"),
+    Permission0004("功能权限删除","Permission0004");
 
     private AuthCode(String name, String index) {
         this.name = name;
